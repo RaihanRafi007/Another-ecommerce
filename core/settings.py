eee85bde318a9282c5309d1997e84d3a6438daa9
RaihanRafi007 <rrafi0570@gamail.com>
2023-02-27 19:36:55 +0600

    build

--- core/settings.py
+++ core/settings.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import os
 import environ
 from datetime import timedelta
+import dj_database_url
 env = environ.Env()
 environ.Env.read_env()
 
@@ -111,11 +112,23 @@ WSGI_APPLICATION = 'core.wsgi.application'
 #     }
 # }
 
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
 DATABASES = {
-    'default': env.db('DATABASE_URL')
+    'default': dj_database_url.config(
+        default=env.db('DATABASE_URL'),
+        conn_max_age=600
+        )
 }
 
-DATABASES["default"]["ATOMIC_REQUESTS"] = True
+# DATABASES["default"]["ATOMIC_REQUESTS"] = True
+
+# DATABASES = {
+#     'default': env.db('DATABASE_URL')
+# }
+
+# DATABASES["default"]["ATOMIC_REQUESTS"] = True
 
 # CORS_ORIGIN_WHITELIST = [
 #     'http://localhost:3000',
@@ -256,9 +269,12 @@ if not DEBUG:
 
 
     DATABASES = {
-        "default": env.db("DATABASE_URL"),
-    }
-    DATABASES["default"]["ATOMIC_REQUESTS"] = True
+    'default': dj_database_url.config(
+        default=env.db('DATABASE_URL'),
+        conn_max_age=600
+        )
+}
+
 
     # # django-ckeditor will not work with S3 through django-storages without this line in settings.py
     # AWS_QUERYSTRING_AUTH = False
